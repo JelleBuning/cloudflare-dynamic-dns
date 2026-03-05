2db70a3acdc5fe9c9198f96b2d5fe474b9b39ebd
Jelle Buning <jellebuning@outlook.com>
2026-03-05 17:24:33 +0100

    Introduced new IpFetcher and created tests

--- src/CloudflareDynamicDns.Core/Fetchers/IpifyFetcher.cs
+++ src/CloudflareDynamicDns.Core/Fetchers/IpifyFetcher.cs
@@ -2,15 +2,14 @@
 
 namespace CloudflareDynamicDns.Core.Fetchers;
 
-public class PublicIpAddressFetcher : IPublicIpAddressFetcher
+public class IpifyFetcher(HttpClient httpClient) : IPublicIpAddressFetcher
 {
     public async Task<string> FetchIpAddressAsync()
     {
         const string url = "https://api.ipify.org";
-        using var client = new HttpClient();
         try
         {
-            var publicIp = await client.GetStringAsync(url);
+            var publicIp = await httpClient.GetStringAsync(url);
             return publicIp.Trim(); 
         }
         catch (HttpRequestException ex)
